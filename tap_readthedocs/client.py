5f7ef1ee963139a482cc201eb0a6af166bca9482
Edgar Ramírez <edgarrm358@gmail.com>
2022-04-18 21:04:03 -0500

    Use paginator class (#13)
    * feat: add paginator class
    
    * fix: handle rate limits
    
    * feat: test a header links paginator
    
    * fix: address code smells
    
    * fix: custom paginator signature
    
    * feat: include HeaderLinkPaginator in paginators library
    
    * fix: rename CustomPaginator -> LegacyStreamPaginator
    
    * fix: import BaseAPIPaginator
    
    * fix: make linter happy
    
    * fix: remove the BasePageSizePaginator abstract base class
    
    * fix: make page number and page size paginators abstract classes
    
    * fix: ABC exception message in Python 3.9
    
    * fix: name paginator arguments explicitly
    
    * fix: use the new extract_jsonpath from the SDK
    
    * fix: use the new extract_jsonpath from the SDK in other places
    
    * fix: use allow-star-arg-any = true

--- tap_readthedocs/client.py
+++ tap_readthedocs/client.py
@@ -1,13 +1,96 @@
 """REST client handling, including ReadTheDocsStream base class."""
 
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Generic, Iterable, Optional, TypeVar
 
 import requests
 import requests_cache
 from singer_sdk.authenticators import APIKeyAuthenticator
+from singer_sdk.exceptions import RetriableAPIError
+from singer_sdk.helpers.jsonpath import extract_jsonpath
 from singer_sdk.streams import RESTStream
 
+from tap_readthedocs.pagination import (
+    BaseAPIPaginator,
+    BaseOffsetPaginator,
+    TPageToken,
+    first,
+)
+
 requests_cache.install_cache()
+TStream = TypeVar("TStream", bound=RESTStream)
+
+
+class LegacyStreamPaginator(
+    BaseAPIPaginator[TPageToken],
+    Generic[TPageToken, TStream],
+):
+    """Paginator that works with REST streams as they exist today."""
+
+    def __init__(
+        self,
+        start_value: TPageToken,
+        stream: TStream,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        """Create a new paginator.
+
+        Args:
+            start_value: Initial value for the paginator
+            stream: A RESTStream instance.
+            args: Paginator positional arguments.
+            kwargs: Paginator keyword arguments.
+        """
+        super().__init__(start_value, *args, **kwargs)
+        self.stream = stream
+
+    def get_next(self, response: requests.Response) -> Optional[TPageToken]:
+        """Get next page value by calling the stream method.
+
+        Args:
+            response: API response object.
+
+        Returns:
+            The next page token or index. Return `None` from this method to indicate
+                the end of pagination.
+        """
+        return self.stream.get_next_page_token(response, self.current_value)
+
+
+class ReadTheDocsPaginator(BaseOffsetPaginator):
+    """Paginator that stops when a page with 0 items is returned."""
+
+    def __init__(self, start_value: int, page_size: int, records_jsonpath: str) -> None:
+        """Create a new paginator.
+
+        Args:
+            start_value: Initial value.
+            page_size: Number of items per page.
+            records_jsonpath: A JSONPath expression.
+        """
+        super().__init__(start_value, page_size)
+        self._records_jsonpath = records_jsonpath
+
+    def has_more(self, response: requests.Response) -> bool:
+        """Check if response has any items.
+
+        Args:
+            response: API response object.
+
+        Returns:
+            True if response contains at least one item.
+        """
+        try:
+            first(
+                extract_jsonpath(
+                    self._records_jsonpath,
+                    response.json(),
+                )
+            )
+        except StopIteration:
+            return False
+
+        return True
 
 
 class ReadTheDocsStream(RESTStream):
@@ -15,6 +98,7 @@ class ReadTheDocsStream(RESTStream):
 
     url_base = "https://readthedocs.org"
     records_jsonpath = "$.results[*]"
+    page_size = 50
 
     @property
     def authenticator(self) -> APIKeyAuthenticator:
@@ -41,6 +125,19 @@ class ReadTheDocsStream(RESTStream):
         headers["User-Agent"] = f"{self.tap_name}/{self._tap.plugin_version}"
         return headers
 
+    def validate_response(self, response: requests.Response) -> None:
+        """Validate HTTP response.
+
+        Args:
+            response: A `requests.Response`_ object.
+
+        Raises:
+            RetriableAPIError: If the rate limit is hit.
+        """
+        if response.status_code == 429:
+            raise RetriableAPIError(response.reason)
+        super().validate_response(response)
+
     def get_url_params(
         self,
         context: Optional[dict],
@@ -56,26 +153,43 @@ class ReadTheDocsStream(RESTStream):
             Mapping of URL query parameters.
         """
         return {
-            "limit": 50,
+            "limit": self.page_size,
             "offset": next_page_token,
             "expand": "config",
         }
 
-    def get_next_page_token(
-        self,
-        response: requests.Response,
-        previous_token: Optional[int],
-    ) -> Optional[int]:
-        """Get next page offset.
-
-        Args:
-            response: API response.
-            previous_token: Previous offset.
+    def get_new_paginator(self) -> ReadTheDocsPaginator:
+        """Get a fresh paginator for this API endpoint.
 
         Returns:
-            Page offset.
+            A paginator instance.
         """
-        if not len(response.json()["results"]):
-            return None
+        return ReadTheDocsPaginator(
+            start_value=0,
+            page_size=self.page_size,
+            records_jsonpath=self.records_jsonpath,
+        )
 
-        return 50 if previous_token is None else previous_token + 50
+    def request_records(self, context: Optional[dict]) -> Iterable[dict]:
+        """Request records from REST endpoint(s), returning response records.
+
+        If pagination is detected, pages will be recursed automatically.
+
+        Args:
+            context: Stream partition or context dictionary.
+
+        Yields:
+            An item for every record in the response.
+        """
+        paginator = self.get_new_paginator()
+        decorated_request = self.request_decorator(self._request)
+
+        while not paginator.finished:
+            prepared_request = self.prepare_request(
+                context,
+                next_page_token=paginator.current_value,
+            )
+            resp = decorated_request(prepared_request, context)
+            yield from self.parse_response(resp)
+
+            paginator.advance(resp)
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,10 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
+from requests import Response
 from singer_sdk.testing import get_standard_tap_tests
 
+from tap_readthedocs.client import ReadTheDocsPaginator
+from tap_readthedocs.streams import ReadTheDocsStream
 from tap_readthedocs.tap import TapReadTheDocs
 
 SAMPLE_CONFIG = {}
@@ -12,3 +15,34 @@ def test_standard_tap_tests():
     tests = get_standard_tap_tests(TapReadTheDocs, config=SAMPLE_CONFIG)
     for test in tests:
         test()
+
+
+def test_paginator():
+    """Validate paginator that uses the page offset."""
+
+    response = Response()
+    paginator = ReadTheDocsPaginator(
+        start_value=0,
+        page_size=2,
+        records_jsonpath=ReadTheDocsStream.records_jsonpath,
+    )
+
+    assert not paginator.finished
+    assert paginator.current_value == 0
+
+    response._content = b'{"results": [{}, {}]}'
+    paginator.advance(response)
+    assert not paginator.finished
+    assert paginator.current_value == 2
+    assert paginator.count == 1
+
+    response._content = b'{"results": [{}, {}]}'
+    paginator.advance(response)
+    assert not paginator.finished
+    assert paginator.current_value == 4
+    assert paginator.count == 2
+
+    response._content = b'{"results": []}'
+    paginator.advance(response)
+    assert paginator.finished
+    assert paginator.count == 3
